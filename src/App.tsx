fbb2bdb1450f144fd206e2ccf0c01830af9095e3
deivisonisidoro <deivisonisidoro@gmail.com>
2022-02-01 09:11:50 -0300

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import User from './components/User';
+import api from './services/api'
 
+interface IUser{
+  name: string;
+  email: string;
+}
 function App() {
+  const [user, setUser] = useState<IUser[]>([]);
+  useEffect(()=>{
+    api.get<IUser[]>('/users').then(response => {
+      setUser(response.data);
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.map(user=> <User key={user.email} user={user}/>)}
     </div>
   );
 }
